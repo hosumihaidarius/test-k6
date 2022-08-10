5d8ce6c79baf466a214a50a2a34f208cc190f8a6
hosumihaidarius <43984799+hosumihaidarius@users.noreply.github.com>
2022-08-10 10:24:26 +0300

    Update k6-test-script.js

--- examples/k6-test-script.js
+++ examples/k6-test-script.js
@@ -1,7 +1,15 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import { printMessage } from './utils.js';
+
+export const options = {
+    teardownTimeout: '200s',
+    vus : 10,
+    duration : '30s',
+}
 
 export default function () {
-  http.get('https://kubeshop.github.io/testkube/');
-  sleep(1);
+    printMessage();
+    http.get('https://kubeshop.github.io/testkube/');
+    sleep(1);
 }
